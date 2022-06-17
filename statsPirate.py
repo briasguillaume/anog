fe1dc4257990111fcd05b75cc2bae941ea3c0767
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-06-17 15:14:20 +0200

    bugfix

--- statsPirate.py
+++ statsPirate.py
@@ -10,4 +10,4 @@ class StatsPirate(object):
 		degats=20*level*(5-qualite)
 		defense=10*level*(5-qualite)
 		fatigue=100*(5-qualite)
-		return [vie+fruitsPower[0], degats+fruitsPower[1], defense+fruitsPower[2], fatigue+fruitsPower[3]]
+		return [vie+fruitpower[0], degats+fruitpower[1], defense+fruitpower[2], fatigue+fruitpower[3]]
