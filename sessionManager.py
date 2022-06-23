e7a96d106c75786dca912cf56f064fbcc46ae7f5
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-06-23 15:10:08 +0200

    trying to fix sessions

--- sessionManager.py
+++ sessionManager.py
@@ -1,6 +1,21 @@
 from menu import Menu
 
-class SessionManager(object):
+class SessionManagerMeta(type):
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class SessionManager(metaclass=SessionManagerMeta):
     
     
     def __init__(self):
