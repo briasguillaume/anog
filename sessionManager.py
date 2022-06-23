6fe8c68aed6f89b10cfa308d35f2c87b73fcb83c
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-06-23 14:29:00 +0200

    trying to fix merge of sessions

--- sessionManager.py
+++ sessionManager.py
@@ -4,27 +4,20 @@ class SessionManager(object):
     
     
     def __init__(self):
-        self._playersID={}
-        self._menusID={}
+        self._players={}
         
     def newSession(self, username):
-        newid=self.maxID()+1
-        self._playersID[username]=newid
-        self._menusID[newid]=Menu()
-        return self._menusID[newid]
+        self._players[username]=Menu()
+        return self._players[username]
         
         
     def session(self, username):
-        if username in self._playersID:
+        if username in self._players:
             return self.getMenu(username)
         else:
             return self.newSession(username)
         
     def getMenu(self, username):
-        return self._menusID[self._playersID[username]]
+        return self._players[username]
     
     
-    def maxID(self):
-        if len(self._playersID)==0:
-            return 1
-        return max(self._playersID.values())
