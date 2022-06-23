34fedbbf1000d97534c90f179e1a15fc6a209361
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-06-23 13:36:45 +0200

    session manager

---
+++ sessionManager.py
@@ -0,0 +1,26 @@
+class SessionManager(object):
+    
+    
+    def __init__(self):
+        self._playersID={}
+        self._menusID={}
+        
+    def newSession(self, username):
+        newid=self.maxID()+1
+        self._playersID[username]=newid
+        self._menusID[newid]=m.Menu()
+        return self._menusID[newid]
+        
+        
+    def session(self, username):
+        if username in self._playersID:
+            return self.getMenu(username)
+        else:
+            return self.newSession(username)
+        
+    def getMenu(self, username):
+        return self._menusID[self._playersID[username]]
+    
+    
+    def maxID():
+        return max(self._playersID.values())
