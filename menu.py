4e1f29275c289da0564153966c259968c148cb6e
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-06-24 10:08:36 +0200

    bugfix

--- menu.py
+++ menu.py
@@ -135,7 +135,7 @@ class Menu(object):
 	
 	def instanciateJoueur(self, username, password):
 		# https://docs.python.org/fr/3/library/hashlib.html
-		password=blake2b(password).hexdigest()
+		password=hashlib.blake2b(password).hexdigest()
 		password=password[0:19]
 		self._joueur=Joueur(username, password)
 		if self._joueur.username==None: #wrong password
