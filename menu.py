48c51b3f56b53446c97607a60a90df857f5690f3
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-06-27 11:54:36 +0200

    login page

--- menu.py
+++ menu.py
@@ -123,7 +123,7 @@ class Menu(object):
 							<title>
 								ANOG
 							</title>
-							<link rel="stylesheet" type="text/css" href="{{ url_for('OnePiece/workspace/python-pipeline/static', filename='style.css')}}">
+							<link rel="stylesheet" type="text/css" href="{{ url_for('/static', filename='style.css')}}">
 						</head>
 						<body>
 							<div id="bg"></div>
