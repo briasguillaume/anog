6d288c960647b782662b2eb1cc114f5bbee9ce2c
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-06-30 11:18:27 +0200

    removed debug

--- stage.py
+++ stage.py
@@ -1,9 +1,6 @@
 
 class Stage(object):
 
-
-	debug=False
-
 	def __init__(self, islands):#, avancee):
 		self._islands=islands
 		#self._avancee=avancee
@@ -13,27 +10,12 @@ class Stage(object):
 	def islands(self):
 		return self._islands
 
-	'''
-	@property
-	def avancee(self):
-		return self._avancee
-	'''
-
 
 	def __str__(self):
-		if Stage.debug:
-			txt="Les prochaines iles sont: \n"
-			count=0
-			for island in self._islands:
-				txt=txt+"Choix "+str(count)+": "+str(island)
-				count+=1
-
-			return txt
-		else:
-			txt="<br>Les prochaines iles sont: <br>"
-			count=0
-			for island in self._islands:
-				txt=txt+"Choix "+str(count)+": "+str(island)+"<br>"
-				count+=1
-			txt=txt+"<br>"
-			return txt
+		txt="<br>Les prochaines iles sont: <br>"
+		count=0
+		for island in self._islands:
+			txt=txt+"Choix "+str(count)+": "+str(island)+"<br>"
+			count+=1
+		txt=txt+"<br>"
+		return txt
