54ef009260990cecac4d05109714a95611b23011
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-06-30 11:16:35 +0200

    removed debug

--- joueur.py
+++ joueur.py
@@ -8,72 +8,32 @@ from island import Island
 
 class Joueur(object):
 
-	debug=False
-
 
 	def __init__(self, username=0, password=0):
 		InteractBDD.cleanUpDB()
-		if Joueur.debug:
-			self._username= username
-			self._equipage= self.getMyCrew()
-			self._position= self.getMyLocation()
-			self._availableToFight=True
-			
-		elif username==0 and password==0:
-
-			self._availableToFight=True
+		if self.existInDB(username):
+			if not InteractBDD.checkPassword(username, password):
+				
+				self._username= None
+				
 		else:
-			if self.existInDB(username):
-				if not InteractBDD.checkPassword(username, password):
-					
-					self._username= None
-					
-			else:
-				self.createNewUser(username, password)
-				InteractBDD.setMyCrew(username, World.carte()[0].islands[0].name, [Pirate(1, True, username)]) 
-			self._username= username
-			self._equipage= self.getMyCrew()
-			self._position= self.getMyLocation()
-			self._availableToFight=True
-			
+			self.createNewUser(username, password)
+			InteractBDD.setMyCrew(username, World.carte()[0].islands[0].name, [Pirate(1, True, username)]) 
+		self._username= username
+		self._equipage= self.getMyCrew()
+		self._position= self.getMyLocation()
+		self._availableToFight=True
+		
 
 	def showMenu(self):
-		if Joueur.debug:
-			Utils.clear()
-			print("Voici ton équipage:\n"+str(self._equipage)+"\n")
-			print("Vous êtes actuellement ici: "+str(self._position)+"\n")
-			World.showMap(self._position.name)
-			nextIsland=World.next(self._position.name)
-			if nextIsland==None:
-				print("GG t'es devenu le roi des pirates")
-				return None
-
-			self._position=nextIsland
-			self._equipage.regenerateHealth()
-			Utils.fight(self._equipage, self._position.pirates)
-			if self._equipage.availableToFight:
-				self.recrutement(5)
-			else:
-				self._equipage= self.getMyCrew()
-				self._position= self.getMyLocation()
-
-				playagain= input("Ton équipage est mort, il va falloir recommencer du début pour devenir le roi des pirates. y/n \n" )
-				while playagain!="y":
-					playagain= input("Ton équipage est mort, il va falloir recommencer du début pour devenir le roi des pirates. y/n \n" )
-				
-
-
-			Utils.clear()
-			self.showMenu()
-
-		else:
-			txt="Voici ton équipage:<br>"+str(self._equipage)+"<br>"
-			txt=txt+"Vous êtes actuellement ici: "+str(self._position)+"<br>"
-			txt=txt+World.showMap(self._position.name)
-			
-			txt=txt+"Dans quelle ile veux-tu aller maintenant? <br>"
-			txt=txt+World.getNextStage(self._position.name)
-			return txt
+		
+		txt="Voici ton équipage:<br>"+str(self._equipage)+"<br>"
+		txt=txt+"Vous êtes actuellement ici: "+str(self._position)+"<br>"
+		txt=txt+World.showMap(self._position.name)
+		
+		txt=txt+"Dans quelle ile veux-tu aller maintenant? <br>"
+		txt=txt+World.getNextStage(self._position.name)
+		return txt
 
 	def isinstance(self):
 		return "Joueur"
@@ -118,23 +78,12 @@ class Joueur(object):
 		return txt
 
 	def recrutement(self, number, pirates=[], value=0):
-		if Joueur.debug:
-			pirates=[]
-			print("Des pirates sont disponibles au recrutement.\n")
-			for i in range(0,number):
-				pirate=Pirate(self._position.level)
-				pirates.append(pirate)
-				print("Choix "+str(i)+": "+str(pirate))
-			value = int(input("Lequel voulez-vous recruter?\n"))
-			if value<number:
-				self._equipage.newFighter(pirates[value])
-
-		else:
-			if int(value)<number:
-				newPirate=pirates[int(value)]
-				self._equipage.newFighter(newPirate)
-				InteractBDD.addNewFighter(self._username, newPirate)
-			return self.showMenu()
+		
+		if int(value)<number:
+			newPirate=pirates[int(value)]
+			self._equipage.newFighter(newPirate)
+			InteractBDD.addNewFighter(self._username, newPirate)
+		return self.showMenu()
 
 
 	def cleanUpDeadPirates(self):
@@ -193,49 +142,34 @@ class Joueur(object):
 		self._equipage=equipage
 
 	def existInDB(self, username):
-		if Joueur.debug:
-			return True
-		else:
-			return InteractBDD.existInDB(username)
+		return InteractBDD.existInDB(username)
 
 
 	def createNewUser(self, username, password):
-		if Joueur.debug:
-			pass
-		else:
-			return InteractBDD.createUser(username, password)
+		return InteractBDD.createUser(username, password)
 
 
 	def checkPassword(self, username, password):
-		if Joueur.debug:
-			return True
-		else:
-			return InteractBDD.checkPassword(username, password)
+		return InteractBDD.checkPassword(username, password)
 
 
 	def getMyCrew(self):
-		if Joueur.debug:
-			return Equipage([Pirate(1, True)])
-		else:
-			txtPirates=InteractBDD.getMyCrew(self._username)
-			if len(txtPirates)==0:
-				pirate=Pirate(1, True, self._username)
-				InteractBDD.setMyCrew(self._username, World.carte()[0].islands[0].name, [pirate])
-				return Equipage([pirate])
+		txtPirates=InteractBDD.getMyCrew(self._username)
+		if len(txtPirates)==0:
+			pirate=Pirate(1, True, self._username)
+			InteractBDD.setMyCrew(self._username, World.carte()[0].islands[0].name, [pirate])
+			return Equipage([pirate])
 
-			else:
-				pirates=[]
-				for txt in txtPirates:
-					pirate=Utils.load(txt)
-					pirates.append(pirate)
-				return Equipage(pirates)
+		else:
+			pirates=[]
+			for txt in txtPirates:
+				pirate=Utils.load(txt)
+				pirates.append(pirate)
+			return Equipage(pirates)
 
 
 	def getMyLocation(self):
-		if Joueur.debug:
-			return World.carte()[0].islands[0]
-		else:
-			return Island(InteractBDD.getMyLocation(self._username),0,0)
+		return Island(InteractBDD.getMyLocation(self._username),0,0)
 	
 
 
