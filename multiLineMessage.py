458fd782aef3c9dccebfb9f5889cdaa6ade65136
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-07-01 14:49:18 +0200

    encapsulation des messages

---
+++ multiLineMessage.py
@@ -0,0 +1,26 @@
+
+from message import Message
+
+
+class MultiLineMessage(object):
+
+
+    def __init__(self, message=Message("")):
+        self._myMessages=[]
+        if message.texte!="":
+            self._myMessages.append([message])
+
+
+    @property
+    def myMessages(self):
+        return self._myMessages
+
+    def __add__(self, messages):
+        if isinstance(messages, Message):
+            self._myMessages.append([messages])
+        elif isinstance(messages, MultiLineMessage):
+            self._myMessages.extend(messages)
+
+
+    def __mul__(self, message):
+        self._myMessages[-1]=self._myMessages[-1].append(message)
