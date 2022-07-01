174b01e12ef03c804c81949a2c71d017dc31879f
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-07-01 09:33:01 +0200

    encapsulation des messages

--- output.py
+++ output.py
@@ -6,33 +6,33 @@ class Output(object):
     def __init__(self):
         self._toBeDisplayed={}
 
-        self._toBeDisplayed['team']=""
-        self._toBeDisplayed['content']=""
-        self._toBeDisplayed['map']=""
+        self._toBeDisplayed['team']=[]
+        self._toBeDisplayed['content']=[]
+        self._toBeDisplayed['map']=[]
 
     @property
     def toBeDisplayed(self):
         return self.transformOutput()
 
     def team(self, txt):
-        self._toBeDisplayed['team']=self._toBeDisplayed['team']+txt
+        self._toBeDisplayed['team'].append(txt)
 
     def content(self, txt):
-        self._toBeDisplayed['content']=self._toBeDisplayed['content']+txt
+        self._toBeDisplayed['content'].append(txt)
 
     def map(self, txt):
-        self._toBeDisplayed['map']=self._toBeDisplayed['map']+txt
+        self._toBeDisplayed['map'].append(txt)
 
     def reset(self):
         for key in self._toBeDisplayed:
-            self._toBeDisplayed[key]=""
-
+            self._toBeDisplayed[key]=[]
+''' 
     def transformOutput(self):
         outputAsArray={}
         for key in self._toBeDisplayed:
             outputAsArray[key]=Output.splitText(self._toBeDisplayed[key])
         return outputAsArray
-
+instead of that we automatically split with array of Message
     @staticmethod
     def splitText(txt):
         if txt!="" and txt!=None:
@@ -41,3 +41,4 @@ class Output(object):
             return txt
         else:
             return ""
+'''
